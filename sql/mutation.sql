4c615d85d20244ef2aab7d89df756441729cc110
Kohpai <whcwhc78@gmail.com>
2020-10-24 20:42:04 +0200

    :lock: omit new_customer()

--- sql/mutation.sql
+++ sql/mutation.sql
@@ -10,9 +10,31 @@ $$
 	STRICT
 	SECURITY DEFINER;
 
-COMMENT ON FUNCTION public.new_customer () IS 'Add a new customer from Firebase user';
+COMMENT ON FUNCTION public.new_customer () IS E'@omit execute\nAdd a new customer from Firebase user';
 
-GRANT EXECUTE ON FUNCTION public.new_customer () TO authuser;
+CREATE OR REPLACE FUNCTION public.current_user()
+    RETURNS public.customer
+AS
+$$
+DECLARE
+    c public.customer;
+BEGIN
+    SELECT *
+    INTO c
+    FROM public.customer
+    WHERE id = current_setting('jwt.claims.firebase_uid', TRUE);
+    IF c IS NULL THEN
+        SELECT public.new_customer()
+        INTO c;
+    END IF;
+    RETURN c;
+END;
+$$
+    LANGUAGE plpgsql
+    SECURITY DEFINER;
+
+COMMENT ON FUNCTION public.current_user() IS 'Get current logged-in user';
+GRANT EXECUTE ON FUNCTION public.current_user() TO authuser;
 
 CREATE OR REPLACE FUNCTION public.top_up(order_id text, amount money)
 	RETURNS public.customer
@@ -32,32 +54,7 @@ COMMENT ON FUNCTION public.top_up (order_id text, amount money) IS 'Top up the c
 
 GRANT EXECUTE ON FUNCTION public.top_up (order_id text, amount money) TO authuser;
 
-CREATE OR REPLACE FUNCTION public.current_user()
-	RETURNS public.customer
-AS
-$$
-DECLARE
-	c public.customer;
-BEGIN
-	SELECT *
-	INTO c
-	FROM public.customer
-	WHERE id = current_setting('jwt.claims.firebase_uid', TRUE);
-	IF c IS NULL THEN
-		SELECT public.new_customer()
-		INTO c;
-	END IF;
-	RETURN c;
-END;
-$$
-	LANGUAGE plpgsql
-	SECURITY DEFINER;
-
-COMMENT ON FUNCTION public.current_user() IS 'Get current logged-in user';
-
-GRANT EXECUTE ON FUNCTION public.current_user() TO authuser;
-
-CREATE OR REPLACE FUNCTION public.parse_page_range(pr text)
+CREATE OR REPLACE FUNCTION public.parse_page_range(page_range text)
 	RETURNS smallint
 AS
 $$
