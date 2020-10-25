9b20c083c8e850f32d2c7292c51ceaad75c1f738
Kohpai <whcwhc78@gmail.com>
2020-10-25 12:39:58 +0100

    :lock: allow more cors origins

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 const allowed = cors({
     origin: [
         'http://localhost:3000',
+        'https://the-prince-de.web.app',
+        'https://the-prince-de.firebaseapp.com',
         'https://the-prince-98130.web.app',
         'https://the-prince-98130.firebaseapp.com',
     ],
