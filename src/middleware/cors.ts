55a284050e8ba2a4edd82828ff45e5fc2adafa91
Kohpai <whcwhc78@gmail.com>
2021-06-11 15:13:13 +0200

    :lock: add allowed origin for theprince.kohpai.me

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,10 +3,9 @@ import cors from 'cors'
 const allowed = cors({
     origin: [
         'http://localhost:3000',
-        'https://the-prince-de.web.app',
-        'https://the-prince-de.firebaseapp.com',
         'https://the-prince-98130.web.app',
         'https://the-prince-98130.firebaseapp.com',
+        'https://theprince.kohpai.me',
     ],
 })
 
